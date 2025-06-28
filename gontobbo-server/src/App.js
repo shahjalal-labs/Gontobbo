6b58551e4c53c7132d1ee5945c120d9b532e2f53
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-06-28 13:08:37 +0600

    ✏️ Updated: gontobbo-server/src/App.js

--- gontobbo-server/src/App.js
+++ gontobbo-server/src/App.js
@@ -1,19 +1,1 @@
-vim.keymap.set("n", "<leader>sp", function()
-  vim.fn.jobstart({
-    "bash", "-c",
-    [[
-f="/run/media/sj/developer/web/L1B11/screenshots/ss-$(LC_TIME=C date +%I-%M-%S_%p-%d-%m-%y).png";
-grim "$f" &&
-wl-copy --type image/png < "$f" &&
-echo "![Screenshot](./screenshots/$(basename "$f"))"
-]]
-  }, {
-    stdout_buffered = true,
-    on_stdout = function(_, data)
-      if data then
-        vim.api.nvim_put(data, "c", true, true)
-      end
-    end
-  })
-end, { desc = "Paste screenshot markdown" })
 
