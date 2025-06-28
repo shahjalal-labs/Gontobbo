0686361393d112ad65b7309e8490212f491e0ee0
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-06-28 20:59:46 +0600

    feat: Added Register/Register.jsx | style: Updated layout/AuthLayout.jsx, Login/Login.jsx, router/authRouter.jsx

--- gontobbo-client/src/layout/AuthLayout.jsx
+++ gontobbo-client/src/layout/AuthLayout.jsx
@@ -1,10 +1,18 @@
+import { Outlet } from "react-router";
 import Navbar from "../pages/shared/Navbar/Navbar";
+import Benefits from "../pages/Home/Benefits/Benefits";
+import { BeMerchant } from "../pages/Home/Home/import";
 
 const AuthLayout = () => {
   return (
     <div>
       <Navbar />
       <h1>AuthLayout</h1>
+      <div className="min-h-[70vh] border">
+        <Outlet />
+      </div>
+      <Benefits />
+      <BeMerchant />
     </div>
   );
 };
--- gontobbo-client/src/router/authRouter.jsx
+++ gontobbo-client/src/router/authRouter.jsx
@@ -1,4 +1,6 @@
 import AuthLayout from "../layout/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
+import Register from "../pages/Authentication/Register/Register";
 
 const authRouter = {
   path: "/auth",
@@ -6,11 +8,11 @@ const authRouter = {
   children: [
     {
       path: "signin",
-      // Component: SignIn,
+      Component: Login,
     },
     {
       path: "signup",
-      // Component: SignUp,
+      Component: Register,
     },
   ],
 };
