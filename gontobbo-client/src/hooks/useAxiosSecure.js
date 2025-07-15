4c5b9b64042276078a513ea0dd9d5419342ae601
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-15 16:56:35 +0600

    improve: Updated console/log.json, hooks/useAxiosSecure.js

--- gontobbo-client/src/hooks/useAxiosSecure.js
+++ gontobbo-client/src/hooks/useAxiosSecure.js
@@ -1,4 +1,37 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+
+export const axiosInstance = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+const useAxiosSecure = () => {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const interceptor = axiosInstance.interceptors.request.use(
+      async (config) => {
+        if (user) {
+          const token = await user.getIdToken(); // ✅ this is the correct ID token
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error),
+    );
+
+    return () => {
+      axiosInstance.interceptors.request.eject(interceptor); // Cleanup
+    };
+  }, [user]);
+
+  return axiosInstance;
+};
+
+export default useAxiosSecure;
+
+/* import axios from "axios";
 import useAuth from "./useAuth";
 
 export const axiosInstance = axios.create({
@@ -21,4 +54,4 @@ const useAxiosSecure = () => {
   return axiosInstance;
 };
 
-export default useAxiosSecure;
+export default useAxiosSecure; */
